7eedcbb5e8b8f12e10751e22d96deef7d6f5413b
Rhaegal222 <rhaegal222@gmail.com>
2022-03-08 16:05:21 +0100

    commit

--- stampa_matrice().py
+++ stampa_matrice().py
@@ -1,6 +1,6 @@
 def stampa_matrice(matrice):
     for i in range(len(matrice)):
-        for j in range(len(matrice[0])):
+        for j in range(len(matrice[i])):
             print(matrice[i][j], ' ', end='')
         print()
 stampa_matrice()
